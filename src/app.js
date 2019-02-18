4b835ef2c60ff5ab9af26a80714c66d33e232c6e
Anton Rozdobudko <arnygora@gmail.com>
2019-02-18 21:20:23 +0200

    add router-dom for navigation

--- src/app.js
+++ src/app.js
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, Switch, NavLink, Link} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+const ExpansDashboard = () => (
+    <div>This Is Dashboard Component</div>
+);
+
+const About = () => (
+    <div>This Is About Component</div>
+);
+const Contact = () => (
+    <div>This Is Contact Component</div>
+);
+const Profile = () => (
+    <div>This Is Profile Component</div>
+);
+const NotFound = () => (
+    <div>404 <Link to='/'>Go Home</Link></div>
+);
+const Header = () => (
+    <header>
+        <h2>Expensive application</h2>
+        <NavLink to='/' activeClassName='is-active' exact>Home</NavLink>
+        <NavLink to='/about' activeClassName='is-active'>About</NavLink>
+        <NavLink to='/contact' activeClassName='is-active'>Contact</NavLink>
+        <NavLink to='/profile' activeClassName='is-active'>Profile</NavLink>
+    </header>
+);
+
+
+const routes = (
+    <Router>
+        <Fragment>
+            <Header/>
+            <Switch>
+                <Route path="/" exact component={ExpansDashboard}/>
+                <Route path="/about" component={About}/>
+                <Route path="/contact" component={Contact}/>
+                <Route path="/profile" component={Profile}/>
+                <Route component={NotFound}/>
+            </Switch>
+        </Fragment>
+    </Router>
+);
+
+
+ReactDOM.render(routes, document.getElementById('app'));
 
