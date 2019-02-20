94ad0b827f93909149e3d240e12d8e646795c415
Anton Rozdobudko <arnygora@gmail.com>
2019-02-20 23:54:33 +0200

    dispatching store

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,7 +1,24 @@
-import { createStore } from 'redux'
+import {createStore} from 'redux'
 
-const store = ((state = { count: 0 }) => {
-
+const store = createStore((state = {count: 10}, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {count: state.count + 1};
+        case 'DECREMENT':
+            return {count: state.count - 1};
+        default:
+            return state
+    }
 });
 
-console.log('101');
+console.log(store.getState());
+
+
+store.dispatch({
+    type: 'DECREMENT'
+});
+
+store.dispatch({
+    type: 'DECREMENT'
+});
+console.log(store.getState());
