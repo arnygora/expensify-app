abaaae9358f15abb44807efe5ee72f2e69ecad06
Anton Rozdobudko <arnygora@gmail.com>
2019-02-24 21:26:54 +0200

    red

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,9 +1,34 @@
 import {createStore, combineReducers} from 'redux'
+import uuid from 'uuid'
+
+// ADD_EXPENSE
+const addExpense = (
+    {
+        description = '',
+        note = '',
+        amount = 0,
+        createdAt = 0
+    } = {}
+) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+});
 
 const expensesReducerDefaultState = [];
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
     switch (action.type) {
+        case 'ADD_EXPENSE':
+            return [
+                ...state,
+                action.expense
+            ];
         default:
             return state;
     }
@@ -38,8 +63,14 @@ const store = createStore(
     })
 );
 
+store.subscribe(() => {
+    console.log(store.getState());
+});
 
-console.log(store.getState());
+const addExpense1 = store.dispatch(addExpense({description: 'Rent', amount: 100}));
+const addExpense2 = store.dispatch(addExpense({description: 'Coffee', note: 'very normal people', amount: 300}));
+
+console.log(addExpense2);
 
 const demoState = {
     expenses: [{
