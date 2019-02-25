363cdc7058a30eac10208c1481badd4f99756130
Anton Rozdobudko <arnygora@gmail.com>
2019-02-25 22:20:29 +0200

    set data

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -70,13 +70,23 @@ const setTextFilter = (text = '') => ({
 // SORT_BY_AMOUNT
 const sortByAmount = () => ({
     type: 'SORT_BY_AMOUNT',
-    sortBy: 'amount'
 });
 
 // SORT_BY_DATE
 const sortByDate = () => ({
     type: 'SORT_BY_DATE',
-    sortBy: 'DATA'
+});
+
+// SET_START_DATE
+const setStartDate = (startDate) => ({
+    type: 'SET_START_DATE',
+    startDate
+});
+
+// SET_END_DATE
+const setEndDate = (endDate) => ({
+    type: 'SET_END_DATE',
+    endDate
 });
 
 const filtersReducerDefaultState = {
@@ -96,17 +106,23 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
         case 'SORT_BY_AMOUNT':
             return {
                 ...state,
-                sortBy: action.sortBy
+                sortBy: 'amount'
             };
         case 'SORT_BY_DATE':
             return {
                 ...state,
-                sortBy: action.sortBy
+                sortBy: 'datetetet'
             };
-        case 'START_DATE':
-            return {startDate: undefined};
-        case 'END_DATE':
-            return {endDate: undefined};
+        case 'SET_START_DATE':
+            return {
+                ...state,
+                startDate: action.startDate
+            };
+        case 'SET_END_DATE':
+            return {
+                ...state,
+                endDate: action.endDate
+            };
         default:
             return state;
     }
@@ -123,17 +139,21 @@ store.subscribe(() => {
     console.log(store.getState());
 });
 
-const addExpense1 = store.dispatch(addExpense({description: 'Rent', amount: 100}));
-const addExpense2 = store.dispatch(addExpense({description: 'Coffee', note: 'very normal people', amount: 300}));
+// const addExpense1 = store.dispatch(addExpense({description: 'Rent', amount: 100}));
+// const addExpense2 = store.dispatch(addExpense({description: 'Coffee', note: 'very normal people', amount: 300}));
+//
+// store.dispatch(removeExpense({id: addExpense1.expense.id}));
+// store.dispatch(editExpense(addExpense2.expense.id, {amount: 500, createdAt: 'today'}));
+//
+// store.dispatch(setTextFilter('added text from dispatch'));
+// store.dispatch(setTextFilter());
+//
+// store.dispatch(sortByAmount());
+// store.dispatch(sortByDate());
 
-store.dispatch(removeExpense({id: addExpense1.expense.id}));
-store.dispatch(editExpense(addExpense2.expense.id, {amount: 500, createdAt: 'today'}));
-
-store.dispatch(setTextFilter('added text from dispatch'));
-store.dispatch(setTextFilter());
-
-store.dispatch(sortByAmount());
-store.dispatch(sortByDate());
+store.dispatch(setStartDate(125));
+store.dispatch(setStartDate());
+store.dispatch(setEndDate(1250));
 
 
 const demoState = {
