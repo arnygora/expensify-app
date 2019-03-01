399c61482e4f4be45dc84a99625f71725a7bc7d4
Anton Rozdobudko <arnygora@gmail.com>
2019-03-01 22:09:43 +0200

    added react-dates component// moment.js lib

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,10 +1,19 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import moment from 'moment';
+import {SingleDatePicker} from 'react-dates';
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+
+// equal new Date()
+const now = moment();
 
 export default class ExpenseForm extends Component {
     state = {
         description: '',
         amount: 0,
-        note: ''
+        note: '',
+        createdAt: moment(),
+        calendarFocused: false
     };
 
     changeDescriptionInput = (e) => {
@@ -24,9 +33,18 @@ export default class ExpenseForm extends Component {
         this.setState(() => ({note}));
     };
 
+    onDateChange = (createdAt) => {
+        this.setState(() => ({createdAt}))
+    };
+
+    onFocusChange = ({ focused }) => {
+        this.setState(() => ({ calendarFocused: focused }))
+    };
+
     render() {
         return (
             <div>
+                {now.locale("uk").format('LLL')}
                 <form action="">
                     <input type="text"
                            placeholder='description'
@@ -37,6 +55,14 @@ export default class ExpenseForm extends Component {
                            value={this.state.amount}
                            placeholder='Amount'
                            onChange={this.changeNumberInput}
+                    />
+                    <SingleDatePicker
+                        date={this.state.createdAt}
+                        onDateChange={this.onDateChange}
+                        focused={this.state.calendarFocused}
+                        onFocusChange={this.onFocusChange}
+                        numberOfMonths={1}
+                        isOutsideRange={()=> false}
                     />
                     <textarea name="note"
                               placeholder='add a note for you expense'
