11089217efa0cf1b5da0e53796eaf943297c7202
Anton Rozdobudko <arnygora@gmail.com>
2019-03-03 10:08:14 +0200

    restore index.html

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -3,13 +3,14 @@ import {connect} from 'react-redux';
 import {removeExpense} from "../actions/expenses";
 
 
-const ExpenseListItem = ({dispatch, id, description, amount, createdAt}) => {
+const ExpenseListItem = ({dispatch, id, description, amount, note, createdAt}) => {
     return (
         <div style={{background: 'cyan', padding: 30}}>
             <p style={{display: 'flex', justifyContent: 'space-around', alignItems: 'center', background: 'indianRed', padding: 15}}>
                 {description}
                 <b>created at {createdAt}</b>
                 <span>amount is {amount}</span>
+                <span>note: {note}</span>
             <button onClick={() => {
                 dispatch(removeExpense({id}))
             }}>Remove</button>
