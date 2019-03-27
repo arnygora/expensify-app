7d123bec2654534b09505ee8ef812a20d19107d1
Anton Rozdobudko <arnygora@gmail.com>
2019-03-27 21:21:29 +0200

    styled login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { startLogin } from "../actions/auth";
+import {connect} from 'react-redux';
+import {startLogin} from "../actions/auth";
 
-  const LoginPage = ({ startLogin }) => (
-        <div>
+const LoginPage = ({startLogin}) => (
+    <div className='box-layout'>
+        <div className="box-layout__box">
+            <h1 className='box-layout__title'>Suppa duppa expensify app</h1>
+            <p>created from udemy course</p>
             <button onClick={startLogin}>Log In</button>
         </div>
-    );
+    </div>
+);
 
 const mapDispatchToProps = (dispatch) => ({
     startLogin: () => dispatch(startLogin())
