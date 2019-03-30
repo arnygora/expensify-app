02cb7bad39bde7a0458fb196a4f4ea8bf7797497
Anton Rozdobudko <arnygora@gmail.com>
2019-03-30 11:08:04 +0200

    added loader

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
 import AppRouter, {history} from './routers/AppRouter';
 import configureStore from './store/configureStore';
+import { LoadingPage } from "./components/LoadingPage";
 import {startSetExpense} from "./actions/expenses";
 import { login, logout } from "./actions/auth";
 import getVisibleExpenses from './selectors/expenses';
@@ -30,7 +31,7 @@ const renderApp = () => {
     }
 };
 
-ReactDOM.render(<p>LOADING....</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
